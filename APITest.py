54c11d12b2fd26019f0b72119298a359f6340513
Cameron1044 <50469780+Cameron1044@users.noreply.github.com>
2019-06-15 20:44:57 -1000

    Update APITest.py

--- APITest.py
+++ APITest.py
@@ -1,7 +1,3 @@
-from geopy.distance import geodesic
-newport_ri = (41.49008, -71.312796)
-cleveland_oh = (41.499498, -81.695391)
-print(geodesic(newport_ri, cleveland_oh).miles)import requests
 import json
 import urllib.parse
 
